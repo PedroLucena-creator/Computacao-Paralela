2a4c53c08241697ef1732808b07a76e8be08fa07
ThiagoAguirre <84483140+ThiagoAguirre@users.noreply.github.com>
2022-05-16 08:20:04 -0300

    Update main.py

--- PROJ1 V2/main.py
+++ PROJ1 V2/main.py
@@ -1,21 +1,23 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
-
-def taylor(In):
+T = 100
+def taylor(x):
   S = 0
   X = 0
-  num = In/2
-  for i in range(int(num)):
-    if i == 0:
-      pass
-    else:
-      S += 1/i
-  for i in range(int(num)):
-    X += 1/num
-    num += 1
+  num = T/2
+  if x == 0:
+    for i in range(int(num)):
+      if i == 0:
+        pass
+      else:
+        S += 1/i
+  elif x == 1:
+    for i in range(int(num)):
+      X += 1/num
+      num += 1
   resultado = S + X
   print(resultado)
 
-pool = ThreadPoolExecutor(1)
+pool = ThreadPoolExecutor(2)
 futures = []
-for x in range(1):
-    futures.append(pool.submit(taylor, 100))
+for x in range(2):
+    futures.append(pool.submit(taylor, x))
